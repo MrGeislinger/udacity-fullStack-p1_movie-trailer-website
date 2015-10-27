7aeb639a8213d5d77451abda61f0611f60b2deae
Victor Loren <VictorLoren@users.noreply.github.com>
2015-10-27 10:50:23 -0700

    Add more movies to entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,7 +5,8 @@ from media import Movie
 movies = [
 	Movie(
 		'Finding Nemo',
-		'http://ia.media-imdb.com/images/M/MV5BMTY1MTg1NDAxOV5BMl5BanBnXkFtZTcwMjg1MDI5Nw@@._V1_SY317_CR0,0,214,317_AL_.jpg',
+		('http://ia.media-imdb.com/images/M/MV5BMTY1MTg1NDAxOV5BMl5BanBnXkFtZT'
+			'cwMjg1MDI5Nw@@._V1_SY317_CR0,0,214,317_AL_.jpg'),
 		'https://youtu.be/SPHfeNgogVs',
 		'''
 			After his son is captured in the Great Barrier Reef and taken to 
@@ -15,12 +16,91 @@ movies = [
 	),
 	Movie(
 		'Ride Along',
-		'http://www.blackfilm.com/read/wp-content/uploads/2014/01/Ride-Along-Poster.jpg',
+		('http://www.blackfilm.com/read/wp-content/uploads/2014/01/'
+			'Ride-Along-Poster.jpg'),
 		'https://www.youtu.be/5klp6rkHIks',
 		'''
 			Security guard Ben must prove himself to his girlfriend's brother, 
 			top cop James. He rides along James on a 24-hour patrol of Atlanta.
 		''',
 		3
+	),
+	Movie(
+		'Jurassic World',
+		('http://ia.media-imdb.com/images/M/MV5BMTQ5MTE0MTk3Nl5BMl5BanBnXkFtZT'
+			'gwMjczMzk2NTE@._V1_SX214_AL_.jpg'),
+		'https://youtu.be/RFinNxS5KN4',
+		'''
+			A new theme park is built on the original site of Jurassic Park.
+			Everything is going well until the park's newest attraction--a
+			genetically modified giant stealth killing machine--escapes
+			containment and goes on a killing spree.
+		''',
+		5
+	),
+	Movie(
+		'Indiana Jones and the Kingdom of the Crystal Skull',
+		('http://ia.media-imdb.com/images/M/MV5BMTIxNDUxNzcyMl5BMl5BanBnXkFtZT'
+			'cwNTgwOTI3MQ@@._V1_SY317_CR0,0,214,317_AL_.jpg'),
+		'https://www.youtu.be/nMhfESAa4tw',
+		'''
+			Famed archaeologist/adventurer Dr. Henry "Indiana" Jones is called 
+			back into action when he becomes entangled in a Soviet plot to 
+			uncover the secret behind mysterious artifacts known as the Crystal 
+			Skulls.
+		''',
+		1
+	),
+	Movie(
+		'The Martian',
+		('http://ia.media-imdb.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZT'
+			'gwODA3OTI4NjE@._V1_SY317_CR0,0,214,317_AL_.jpg'),
+		'https://www.youtu.be/ej3ioOneTy8',
+		'''
+			During a manned mission to Mars, Astronaut Mark Watney is presumed 
+			dead after a fierce storm and left behind by his crew. But Watney 
+			has survived and finds himself stranded and alone on the hostile 
+			planet. With only meager supplies, he must draw upon his ingenuity, 
+			wit and spirit to subsist and find a way to signal to Earth that he 
+			is alive.
+		''',
+		3
+	),
+	Movie(
+		'Back to the Future',
+		('http://ia.media-imdb.com/images/M/MV5BMjA5NTYzMDMyM15BMl5BanBnXkFtZT'
+			'gwNjU3NDU2MTE@._V1_SX214_AL_.jpg'),
+		'https://www.youtu.be/yosuvf7Unmg',
+		'''
+			A young man is accidentally sent 30 years into the past in a 
+			time-traveling DeLorean invented by his friend, Dr. Emmett Brown, 
+			and must make sure his high-school-age parents unite in order to 
+			save his own existence.
+		''',
+		5
+	),
+	Movie(
+		'Back to the Future Part II',
+		('http://ia.media-imdb.com/images/M/MV5BNjE5ODg4NzY1MF5BMl5BanBnXkFtZT'
+			'cwMjI3MzYwMg@@._V1_SY317_CR9,0,214,317_AL_.jpg'),
+		'https://www.youtu.be/MdENmefJRpw',
+		'''
+			After visiting 2015, Marty McFly must repeat his visit to 1955 to 
+			prevent disastrous changes to 1985... without interfering with his 
+			first trip.
+		''',
+		4
+	),
+	Movie(
+		'Back to the Future Part III',
+		('http://ia.media-imdb.com/images/M/MV5BMTk3ODgyMTgzNF5BMl5BanBnXkFtZT'
+			'cwODE3MzYwMg@@._V1_SY317_CR9,0,214,317_AL_.jpg'),
+		'https://www.youtu.be/3C8c3EoEfw4',
+		'''
+			Enjoying a peaceable existence in 1885, Doctor Emmet Brown is about 
+			to be killed by Buford "Mad Dog" Tannen. Marty McFly travels back in
+			time to save his friend.
+		''',
+		2
 	)
 ]
